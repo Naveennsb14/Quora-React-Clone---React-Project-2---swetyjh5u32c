d9c0702ac2e8e696163317f4cb46655af84e9ae2
Naveen <baghelnaveensingh@gmail.com>
2023-12-15 15:33:50 +0530

    added heade

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,17 +1,18 @@
+import "./header.css"
 import { MdHome } from "react-icons/md";
 import { FaRegListAlt } from "react-icons/fa";
 import { BsPencilSquare } from "react-icons/bs";
 import { LuUsers } from "react-icons/lu";
 import { IoNotificationsOutline } from "react-icons/io5";
 import { IoIosSearch } from "react-icons/io";
+import { RxAvatar } from "react-icons/rx";
 
 export const Header = () => {
   return (
     <div className='qHeader'>
-        <div className="qHeader-content">
-            <div className='qHeader__logo'>
-                <img src='/images/Quora-Logo-1.jpg' alt='logo'/>
-            </div>
+        <div className="qHeader__content">
+                <img src='/images/quora-new-logo.jpg' alt='logo' className="quora__logo"/>
+            
             <div className="qHeader__icons">
             <div className="qHeader__icon">
             <MdHome />
@@ -30,8 +31,16 @@ export const Header = () => {
             </div>
             </div>
             <div className="qHeader__input">
-            <IoIosSearch /> 
-            <input type="text" placeholder="Search Quora"/>
+            <IoIosSearch className="qHeader__searchlogo"/> 
+            <input type="text" placeholder="Search Quora" className="qHeader__inputbox"/>
+            </div>
+            <div className="qHeader__Rem">
+            <RxAvatar className="qHeader__userlogo"/>
+            <div className="qHeader__addQcreateQ">
+              <button className="addQ">Add question</button>
+              <button className="createQ">Create post</button>
+            </div>
+
             </div>
         </div>
     </div>
