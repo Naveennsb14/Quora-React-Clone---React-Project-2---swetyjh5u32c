b17520dbc42cadaad55ad4ce5251e34e01bfec6d
Naveen <baghelnaveensingh@gmail.com>
2023-12-20 02:45:07 +0530

    added loginpage

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,26 @@
+import Login from "../pages/login/Login";
 import { Quora } from "../pages/quora/Quora";
+import Signup from "../pages/signup/Signup";
 import "../styles/App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-  return <div className="App"><Quora/></div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <div className="App">
+              <Quora />
+            </div>
+          }
+        />
+        <Route path="/signup" element={<Signup/>}/>
+        <Route path="/login" element={<Login/>}/>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
